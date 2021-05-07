2dff3d9da771f284a3d9f1548ee8746838786e93
Ravil Galeyev <dehasi@yandex.ru>
2021-05-07 10:49:30 +0200

    Test names

--- test/world_test.rb
+++ test/world_test.rb
@@ -4,12 +4,23 @@ class Ch2Test < Test::Unit::TestCase
 
   def test_a_new_world_is_empty
     world = World.new
-    assert_equal 0, world.living_cells.count
+
+    assert_true world.empty?
   end
 
   def test_a_cell_can_be_added_to_the_world
     world = World.new
+
     world.set_living_at(1, 1)
-    assert_equal 1, world.living_cells.count
+
+    assert_true world.alive_at?(1, 1)
+  end
+
+  def test_after_adding_a_cell_the_world_is_not_empty
+    world = World.new
+
+    world.set_living_at(1, 1)
+
+    assert_false world.empty?
   end
 end
